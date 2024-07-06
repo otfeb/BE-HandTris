c5635e8444148da327e0c0bbb77a3d70b30181ed
thun0514 <xogns2301@gmail.com>
2024-07-06 23:12:22 +0900

    refactor: Nickname 설정시 한글도 가능하게 추가

--- src/main/java/jungle/HandTris/presentation/dto/request/MemberRequest.java
+++ src/main/java/jungle/HandTris/presentation/dto/request/MemberRequest.java
@@ -17,7 +17,7 @@ public record MemberRequest(@NotBlank(message = "아이디는 필수 입력 값�
 
                             @NotBlank(message = "닉네임은 필수 입력 값입니다.")
                             @Size(min = 4, max = 20, message = "닉네임은 최소 4자 이상, 20자 이하여야 합니다")
-                            @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "닉네임은 알파벳 대소문자와 숫자만 사용 가능합니다.")
+                            @Pattern(regexp = "^[a-zA-Z0-9가-힣]+$", message = "닉네임은 알파벳 대소문자, 숫자, 한글만 사용 가능합니다.")
                             @Pattern(regexp = "^((?!admin).)*$", message = "admin은 사용할 수 없습니다.")
                             String nickname) {
 
